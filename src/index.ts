e5898928c1dbdbf10f7d4ca4372e53c864571bd0
aaron4415 <kinglaam3@gmail.com>
2024-05-15 14:02:45 +0800

    add async await

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { userRecordroute } from "./routes/userRecordroutes";
 import "reflect-metadata";
 import { errorHandler } from "./middlewares/error.middleware";
 import cors from "cors";
-import { DataSource } from "typeorm";
 dotenv.config();
 
 const app = express();
@@ -18,23 +17,17 @@ const { PORT } = process.env;
 // app.get("*", (req: Request, res: Response) => {
 //   res.status(505).json({ message: "Bad Request" });
 // });
-
-AppDataSource.initialize()
-  .then(async () => {
-    console.log("Connection initialized with database...");
-  })
-  .catch((error) => console.log(error));
-
-export const getDataSource = (delay = 3000): Promise<DataSource> => {
-  if (AppDataSource.isInitialized) return Promise.resolve(AppDataSource);
-
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (AppDataSource.isInitialized) resolve(AppDataSource);
-      else reject("Failed to create connection with database");
-    }, delay);
-  });
-};
+async function initDbConnection() {
+  await AppDataSource.initialize()
+    .then(async () => {
+      app.listen(PORT, () => {
+        console.log("Server is running on http://localhost:" + PORT);
+      });
+      console.log("Data Source has been initialized!");
+    })
+    .catch((error) => console.log(error));
+}
+initDbConnection();
 
 app.use("/", userRecordroute);
 
